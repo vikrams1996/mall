2627e4da6742b488960992d4c862421bc88b37f8
Administrator <Administrator@HP>
2018-06-14 11:54:13 +0530

    Add a Unit

---
+++ mallform/Controllers/UnitController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mallform.Models;
+using mallform.ViewModel;
+using System.Data.Entity;
+
+namespace mallform.Controllers
+{
+    public class UnitController : Controller
+    {
+        private ApplicationDbContext _Context;
+
+        public UnitController()
+        {
+
+            _Context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public ActionResult Units()
+        {
+
+            var viewModel = new UnitsFormViewModel
+            {
+                Floors = _Context.Unit.ToList()
+        };
+        
+            return View(viewModel);
+    }
+    
+
+     
+
+    }
+}
---
+++ mallform/ViewModel/UnitsFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mallform.Models;
+
+namespace mallform.ViewModel
+{
+    public class UnitsFormViewModel
+    {
+
+        public string Size { get; set; }
+
+        public int Floor { get; set; }
+      
+        public IEnumerable<Unit> Floors { get; set; }
+
+
+    }
+}
